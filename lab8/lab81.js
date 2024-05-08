2c53a47fdadcb2d6f03b72c9f1b190adc934cdc3
Felipe Reichert <lipereichert94@gmail.com>
2024-05-08 16:08:39 -0500

    updating

--- lab8/lab81.js
+++ lab8/lab81.js
@@ -1,186 +1,171 @@
-class Node {
-    // constructor
-    constructor(element) {
+// references https://medium.com/@andrewsouthard1/linked-list-in-javascript-634fc2e0897b 
+
+function LinkedList() {
+
+    head = null;
+    length = 0;
+
+    var Node = function (element) {
         this.element = element;
-        this.next = null
-    }
-}
-// linkedlist class
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.size = 0;
+        this.next = null;
     }
 
-    // adds an element at the end
-    // of list
-    add(element) {
-        // creates a new node
-        let node = new Node(element);
+    this.head = function () {
+        return head;
+    }
 
-        // to store current node
-        let current;
+    this.length = function () {
+        return length;
+    }
 
-        // if list is Empty add the
-        // element and make it head
-        if (this.head == null)
-            this.head = node;
-        else {
-            current = this.head;
+    this.add = function (element) {
+        var newNode = new Node(element);
 
-            // iterate to the end of the
-            // list
-            while (current.next) {
-                current = current.next;
+        if (head === null) {
+            head = newNode;
+        } else {
+            var currentNode = head;
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
             }
-
-            // add node
-            current.next = node;
+            currentNode.next = newNode;
         }
-        this.size++;
+        length++;
     }
 
-    // insert element at the position index
-    // of the list
-    insertAt(element, index) {
-        if (index < 0 || index > this.size)
-            return console.log("Please enter a valid index.");
-        else {
-            // creates a new node
-            let node = new Node(element);
-            let curr, prev;
-
-            curr = this.head;
-
-            // add the element to the
-            // first index
-            if (index == 0) {
-                node.next = this.head;
-                this.head = node;
+    this.remove = function (element) {
+        if (head === null) {
+            return "empty linked list, nothing to remove";
+        } else {
+            var currentNode = head;
+            if (currentNode.element === element) {
+                head = currentNode.next;
+                length--;
+                return head;
             } else {
-                curr = this.head;
-                let it = 0;
-
-                // iterate over the list to find
-                // the position to insert
-                while (it < index) {
-                    it++;
-                    prev = curr;
-                    curr = curr.next;
+                while (currentNode.next !== null) {
+                    if (currentNode.next.element === element) {
+                        currentNode.next = currentNode.next.next;
+                        length--;
+                        return head;
+                    }
+                    currentNode = currentNode.next;
                 }
-
-                // adding an element
-                node.next = curr;
-                prev.next = node;
             }
-            this.size++;
         }
     }
 
-    // removes an element from the
-    // specified location
-    removeFrom(index) {
-        if (index < 0 || index >= this.size)
-            return console.log("Please Enter a valid index");
+    this.find = function (element) {
+        currentNode = head;
+        position = 1;
+        if (head === null) {
+            return "empty linked linked list";
+        }
         else {
-            let curr, prev, it = 0;
-            curr = this.head;
-            prev = curr;
-
-            // deleting first element
-            if (index === 0) {
-                this.head = curr.next;
+            if (currentNode.element === element) {
+                return position;
             } else {
-                // iterate over the list to the
-                // position to remove an element
-                while (it < index) {
-                    it++;
-                    prev = curr;
-                    curr = curr.next;
+                while (currentNode.element !== element) {
+                    currentNode = currentNode.next;
+                    position++;
                 }
-
-                // remove the element
-                prev.next = curr.next;
-            }
-            this.size--;
-
-            // return the remove element
-            return curr.element;
-        }
-    }
-
-    // removes a given element from the
-    // list
-    removeElement(element) {
-        let current = this.head;
-        let prev = null;
-
-        // iterate over the list
-        while (current != null) {
-            // comparing element with current
-            // element if found then remove the
-            // and return true
-            if (current.element === element) {
-                if (prev == null) {
-                    this.head = current.next;
+                if (currentNode.element === element) {
+                    return position;
                 } else {
-                    prev.next = current.next;
+                    console.log("this hits");
+                    return -1;
                 }
-                this.size--;
-                return current.element;
             }
-            prev = current;
-            current = current.next;
         }
-        return -1;
+    }
+} function LinkedList() {
+
+    head = null;
+    length = 0;
+
+    var Node = function (element) {
+        this.element = element;
+        this.next = null;
     }
 
+    this.head = function () {
+        return head;
+    }
 
-    // finds the index of element
-    indexOf(element) {
-        let count = 0;
-        let current = this.head;
+    this.length = function () {
+        return length;
+    }
 
-        // iterate over the list
-        while (current != null) {
-            // compare each element of the list
-            // with given element
-            if (current.element === element)
-                return count;
-            count++;
-            current = current.next;
+    this.add = function (element) {
+        var newNode = new Node(element);
+
+        if (head === null) {
+            head = newNode;
+        } else {
+            var currentNode = head;
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        length++;
+    }
+
+    this.remove = function (element) {
+        if (head === null) {
+            return "empty linked list, nothing to remove";
+        } else {
+            var currentNode = head;
+            if (currentNode.element === element) {
+                head = currentNode.next;
+                length--;
+                return head;
+            } else {
+                while (currentNode.next !== null) {
+                    if (currentNode.next.element === element) {
+                        currentNode.next = currentNode.next.next;
+                        length--;
+                        return head;
+                    }
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+    }
+
+    this.find = function (element) {
+        currentNode = head;
+        position = 1;
+        if (head === null) {
+            return "empty linked linked list";
+        }
+        else {
+            if (currentNode.element === element) {
+                return position;
+            } else {
+                while (currentNode.element !== element) {
+                    currentNode = currentNode.next;
+                    position++;
+                }
+                if (currentNode.element === element) {
+                    return position;
+                } else {
+                    console.log("this hits");
+                    return -1;
+                }
+            }
         }
 
-        // not found
-        return -1;
     }
-
-    // checks the list for empty
-    isEmpty() {
-        return this.size == 0;
-    }
-
-    // gives the size of the list
-    size_of_list() {
-        console.log(this.size);
-    }
-
-
-    // prints the list items
-    printList() {
-        let curr = this.head;
-        let str = "";
-        while (curr) {
-            str += curr.element + " ";
-            curr = curr.next;
+    this.printList = function () {
+        currentNode = head;
+        while (currentNode != null) {
+            console.log(currentNode.element);
+            currentNode = currentNode.next;
         }
-        console.log(str);
     }
-
 }
 
-// creating an object for the
-// Linkedlist class
 let linkedlist = new LinkedList();
 
 linkedlist.add(1);
@@ -191,6 +176,7 @@ linkedlist.add(3);
 
 linkedlist.printList(); //Expected Result: LinkedList{1,2,3};
 
-linkedlist.removeElement(2);
+//linkedlist.removeElement(2);]
+linkedlist.remove(2);
 
 linkedlist.printList(); //Expected Result: LinkedList{1,3};
